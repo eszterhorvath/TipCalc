b4ed504bf6e51937577e0453e3f4949e4273007a
Eszter <eszter.horvath@opti-q.com>
2020-10-19 11:31:23 +0200

    Added App.cs to the Core project

---
+++ TipCalc.Core/App.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MvvmCross;
+using MvvmCross.ViewModels;
+using TipCalc.Core.Services;
+using TipCalc.Core.ViewModels;
+
+namespace TipCalc.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            // it tells the IoC Container that whenever any code requests an ICalculationService reference,
+            // an object of type CalculationService should be created and returned
+            Mvx.IoCProvider.RegisterType<ICalculationService, CalculationService>();
+
+            RegisterAppStart<TipViewModel>();
+        }
+    }
+}
