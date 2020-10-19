f980d2f63e4fce774c6f788077b512cef74bdfd3
Eszter <eszter.horvath@opti-q.com>
2020-10-19 10:24:53 +0200

    Added TipViewModel.cs

---
+++ TipCalc.Core/ViewModels/TipViewModel.cs
@@ -0,0 +1,73 @@
+﻿using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using TipCalc.Core.Services;
+
+namespace TipCalc.Core.ViewModels
+{
+    public class TipViewModel : MvxViewModel
+    {
+        private readonly ICalculationService _calculationService;
+
+        // dependency injection
+        public TipViewModel(ICalculationService calculationService)
+        {
+            _calculationService = calculationService;
+        }
+
+        private double _subTotal;
+        public double SubTotal
+        {
+            get => _subTotal;
+            set
+            {
+                _subTotal = value;
+                RaisePropertyChanged(() => SubTotal);
+
+                Recalculate();
+            }
+        }
+
+        private int _generosity;
+        public int Generosity
+        {
+            get => _generosity;
+            set
+            {
+                _generosity = value;
+                RaisePropertyChanged(() => Generosity);
+
+                Recalculate();
+            }
+        }
+
+        private double _tip;
+        public double Tip
+        {
+            get => _tip;
+            set
+            {
+                _tip = value;
+                RaisePropertyChanged(() => Tip);
+            }
+        }
+
+        public override async Task Initialize()
+        {
+            await base.Initialize();
+
+            _subTotal = 100;
+            _generosity = 10;
+
+            Recalculate();
+        }
+
+        private void Recalculate()
+        {
+            Tip = _calculationService.TipAmount(SubTotal, Generosity);
+        }
+    }
+}
